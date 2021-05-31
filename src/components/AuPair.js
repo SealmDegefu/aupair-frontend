b579c4fd126de1c93bd76f84c0935fe9f741bd99
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-05-31 10:09:25 -0500

    some base code added to aupair and hostfamily

--- src/components/AuPair.js
+++ src/components/AuPair.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 const AuPair = () => {
 	return (
-		<div>
-			
-		</div>
-	)
+        <div className="card" key={this.props.aupair.id}>
+            <img 
+                className="family-img"
+                src={this.props.aupair.images} 
+                alt="family"
+            />
+			{/* need to go over what we want included */}
+            <a href={this.props.aupair.url} target="_blank">
+                <h2>
+                    {this.props.aupair.fullName}
+                </h2>
+            </a>
+            {/* <h3 className="states">
+                {this.props.park.states}
+            </h3> */}
+            <p>
+                {/* {this.props.family.description} */}
+            </p>
+            {/* <button className="like-btn"onClick={()=>this.props.handleClick(this.props.family)}>Favorite ⭐️ </button> */}
+        </div>
+        );
 }
 
 export default AuPair
+
--- src/components/HostFamily.js
+++ src/components/HostFamily.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-const HostFamily = () => {
-	return (
-		<div>
-			
-		</div>
-	)
+
+class HostFamily extends React.Component {
+
+    
+    render(){
+        return (
+        <div className="card" key={this.props.family.id}>
+            <img 
+                className="family-img"
+                src={this.props.family.images} 
+                alt="family"
+            />
+			{/* need to go over what we want included */}
+            <a href={this.props.family.url} target="_blank">
+                <h2>
+                    {this.props.family.fullName}
+                </h2>
+            </a>
+            {/* <h3 className="states">
+                {this.props.park.states}
+            </h3> */}
+            <p>
+                {/* {this.props.family.description} */}
+            </p>
+            {/* <button className="like-btn"onClick={()=>this.props.handleClick(this.props.family)}>Favorite ⭐️ </button> */}
+        </div>
+        );
+    }
 }
 
 export default HostFamily
