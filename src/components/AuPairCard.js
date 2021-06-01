c661ee17d2b284ea30b3dc6fd237bcad29edd04f
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-06-01 16:56:53 -0500

    updated cards

--- src/components/AuPairCard.js
+++ src/components/AuPairCard.js
@@ -6,9 +6,9 @@ class AuPairCard extends Component {
     return (
       <div className="aupair-card" id={this.props.auPair.id}>
         <h2>{this.props.auPair.name}</h2>
-        <p>{this.props.auPair.age} Age: </p>
-        <p>{this.props.auPair.years_of_experience} Years of Experience: </p>
-        <p>{this.props.auPair.nationality} Nationality:</p>
+        <p>Age: {this.props.auPair.age} </p>
+        <p>Years of Experience:{this.props.auPair.years_of_experience}  </p>
+        <p>Nationality:{this.props.auPair.nationality} </p>
       </div>
     );
   }
--- src/components/HostFamilyCard.js
+++ src/components/HostFamilyCard.js
@@ -6,8 +6,8 @@ class HostFamilyCard extends Component {
     return (
       <div className="HostFamily-card" id={this.props.hostFamily.id}>
         <h2>{this.props.hostFamily.lastName}</h2>
-        <p>{this.props.hostFamily.number_of_kids} Number of Children: </p>
-        <p>{this.props.hostFamily.location} Location:</p>
+        <p>Number of Children: {this.props.hostFamily.number_of_kids}  </p>
+        <p>Location: {this.props.hostFamily.location} </p>
       </div>
     );
   }
