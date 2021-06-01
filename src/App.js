c191a0f58d09f4cf47ebf8789d36d872289b6bd0
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-06-01 13:49:39 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/index';
+import AuPair from './containers/AuPair';
+import HostFamily from './containers/HostFamily';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,14 +13,43 @@ import Header from './components/Navbar/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Home from './components/Home';
-import HostFamily from './components/HostFamily';
 import CreateListing from './components/CreateListing';
-import AuPair from './components/AuPair'
+import axios from 'axios';
+
+
+const aupairsURL = "http://localhost:9292/aupairs";
+const hostFamiliesURL = "http://localhost:9292/hostFamilies";
+
 function App() {
 
+  state = {
+    aupairs: [],
+    hostFamilies: []
+  }
+
+handleAuPairs = (auPairData) => {
+  this.setState({
+    aupairs: auPairData
+  })
+}
+handleHostFamilies = (hostFamilyData) => {
+  this.setState({
+    hostFamilies: hostFamilyData
+  })
+}
+
+componentDidMount = () => {
+  axios.get(aupairsURL)
+    .then((response) => this.handleAuPairs(response.data.data))
+
+  axios.get(favoritesURL)
+    .then(response => this.handleFavoritesData(response.data))
+}
   fetch("http://localhost:9292/test")
   .then((res) => res.json())
   .then(console.log); 
+
+
 
   return (
     <Router>
