37a10e936506f5736a3701520680c96579688106
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-06-03 10:40:24 -0500

    updated About component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 function About() {
 		const [name, setName] = useState('Your Name');
 		const [role, setRole] = useState('Role');
-		// const [name, setAbout] = useState('Your Name')
-		const [name, setAbout] = useState('Born and raised in Addis Ababa, Ethiopia, Selam understands the core need in raising children in a diverse household. Her passion for connecting people and bridging cultural differences is what led her alongside her two co-founders to start Au Paird. When she is not busy leading her team, you can find her by the water enjoying time with family and friends.')
+		// const [name, setAbout] = useState('Your Name')//react useState 
+		const [name, setAbout] = useState('Born and raised in Addis Ababa, Ethiopia, Selam understands the core need in raising children in a diverse household. Her passion for connecting people and bridging cultural differences is what led her alongside her two co-founders to start Au Paird. When she is not busy leading her team, you can find her by the water enjoying time with family and friends.');
 
 
 
@@ -16,9 +16,9 @@ function About() {
 				</div>
 			</div>
 			<div className="lower-container">
-				<h3>( name )</h3>
-				<h4>( role )</h4>
-				<p> ( about ) </p>
+				<h3>{ name }</h3>
+				<h4>{ role }</h4>
+				<p> { about } </p>
 			</div>
 		</div>
 	)
